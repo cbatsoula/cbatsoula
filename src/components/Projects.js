2eebebcaeffc742fff32b8bfd8b2ddbb82ccc6a8
cbatsoula <cbatsoula@gmail.com>
2019-09-22 15:26:57 -0400

    workin

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Projects extends React.Component {
+  render () {
+    return (
+      <div className="Projects">
+      WavesOn
+      _explore_
+      SKINN
+      Card Game
+      </div>
+    );
+  }
+}
+
+export default Projects;
