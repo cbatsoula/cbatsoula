576a683c8cb87d4c89e2a112831f5f8dea19c16c
cbatsoula <cbatsoula@gmail.com>
2019-12-14 12:43:21 -0500

    typo

--- src/containers/About.js
+++ src/containers/About.js
@@ -12,7 +12,7 @@ class About extends React.Component {
 
         </div>
         <div className="About-cont">
-        Full stack web developer with 4 years’ experience in the service industry with a passion for designing projects and coding. Leveraging experience in Ruby on Rails, JavaScript, React and a background in catering, I know how to work on a team under pressure to deliver a successful end result. I bring strong skills in team-building and project management to help drive the project to completion. I discovered coding through meeting new people who encouraged me to take the leap into programming. I am excited to build and maintain web applications that will be used by everyday people navigating through the uncertain waters of life.
+        Full Stack Web Developer with 4 years experience in the service industry with a passion for designing projects and coding. Leveraging experience in Ruby on Rails, JavaScript, React and a background in catering, I know how to work on a team under pressure to deliver a successful end result. I bring strong skills in team-building and project management to help drive the project to completion. I discovered coding through meeting new people who encouraged me to take the leap into programming. I am excited to build and maintain web applications that will be used by everyday people navigating through the uncertain waters of life.
         </div>
       </div>
       </>
